2b2c79c54386f8b14e5f57a8b425f076cd175d52
Tsubasa Oshima <w.tsubasa.physics@gmail.com>
2019-02-08 23:58:13 +0900

    fix

--- server/app.js
+++ server/app.js
@@ -22,27 +22,30 @@ app.post('/recorder/v1/start', (req, res, next) => {
     // const recordStartCommand = `/home/oshima/withlive-agora-recording/samples/cpp/recorder_local --appId ${appid} --uid 0 --channel ${channel} --recordFileRootDir /home/oshima/withlive-agora-recording/server/output --appliteDir  /home/oshima/withlive-agora-recording/bin  --idle=10 --isMixingEnabled=1 --layoutMode=1 &`
     const process = childProcess.spawn('/home/oshima/withlive-agora-recording/samples/cpp/recorder_local', ['--appId', appid, '--uid',  0,  '--channel', channel,  '--recordFileRootDir',  '/home/oshima/withlive-agora-recording/server/output',  '--appliteDir', '/home/oshima/withlive-agora-recording/bin', '--idle=10', '--isMixingEnabled=1', '--layoutMode=1'])
     
-    process.stdout.on('data', (data) => {
-        console.log(`stdout: ${data}`)
-        /*
-        res.status(200).json({
-            success: true
+    try {
+        process.stdout.on('data', (data) => {
+            console.log(`stdout: ${data}`)
+            res.status(200).json({
+                success: true
+            })
         })
-        */
-    })
-      
-    process.stderr.on('data', (data) => {
-        console.log(`stderr: ${data}`)
-        /*
-        res.status(500).json({
-            success: false
+        
+        process.stderr.on('data', (data) => {
+            console.log(`stderr: ${data}`)
+            /*
+            res.status(500).json({
+                success: false
+            })
+            */
         })
-        */
-    })
-    
-    process.on('close', (code) => {
-        console.log(`child process exited with code ${code}`)
-    })
+        
+        process.on('close', (code) => {
+            console.log(`child process exited with code ${code}`)
+        })
+
+    } catch (error){
+        console.log(error)
+    }
 
     /*
     const process = childProcess.execFile('/home/oshima/withlive-agora-recording/samples/cpp/recorder_local', 
