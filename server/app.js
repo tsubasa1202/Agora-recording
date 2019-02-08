a23bb8e7d5f73c0a036dc8de6ddd5af3ee130751
Tsubasa Oshima <w.tsubasa.physics@gmail.com>
2019-02-08 23:46:40 +0900

    spawnに変更

--- server/app.js
+++ server/app.js
@@ -3,8 +3,8 @@ const app = express()
 const port = 80
 // const RecordManager = require('./recordManager')
 const bodyParser = require('body-parser')
-const { exec } = require('child_process')
-// const childProcess = require('child_process');
+// const { exec } = require('child_process')
+const childProcess = require('child_process');
 
 app.use(bodyParser.json());
 
@@ -19,30 +19,32 @@ app.post('/recorder/v1/start', (req, res, next) => {
         throw new Error("channel is mandatory");
     }
     
-    const recordStartCommand = `/home/oshima/withlive-agora-recording/samples/cpp/recorder_local --appId ${appid} --uid 0 --channel ${channel} --recordFileRootDir /home/oshima/withlive-agora-recording/server/output --appliteDir  /home/oshima/withlive-agora-recording/bin  --idle=10 --isMixingEnabled=1 --layoutMode=1`
+    // const recordStartCommand = `/home/oshima/withlive-agora-recording/samples/cpp/recorder_local --appId ${appid} --uid 0 --channel ${channel} --recordFileRootDir /home/oshima/withlive-agora-recording/server/output --appliteDir  /home/oshima/withlive-agora-recording/bin  --idle=10 --isMixingEnabled=1 --layoutMode=1 &`
+    const process = childProcess.spawn('/home/oshima/withlive-agora-recording/samples/cpp/recorder_local', ['--appId', appid, '--uid',  0,  '--channel', channel,  '--recordFileRootDir',  '/home/oshima/withlive-agora-recording/server/output',  '--appliteDir', '/home/oshima/withlive-agora-recording/bin', '--idle=10', '--isMixingEnabled=1', '--layoutMode=1'])
+    
+    process.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`)
+        /*
+        res.status(200).json({
+            success: true
+        })
+        */
+    })
+      
+    process.stderr.on('data', (data) => {
+        console.log(`stderr: ${data}`)
+        /*
+        res.status(500).json({
+            success: false
+        })
+        */
+    })
+    
+    process.on('close', (code) => {
+        console.log(`child process exited with code ${code}`)
+    })
     
     /*
-    const process = childProcess.execFile('/home/oshima/withlive-agora-recording/samples/cpp/recorder_local', 
-    ['--appId',appid, '--uid',  0,  '--channel', channel,  '--recordFileRootDir',
-    '/home/oshima/withlive-agora-recording/server/output',  '--appliteDir',
-    '/home/oshima/withlive-agora-recording/bin', '--idle=10', '--isMixingEnabled=1', '--layoutMode=1', '&'], (error, stdout, stderr) => {
-        
-        if (error) {
-            console.error("stderr", stderr)
-            return res.status(500).json({
-                success: false,
-                pid: process.pid
-            })
-        }
-        // console.log(stdout);
-        // console.log("pid: " +process.pid)
-        return res.status(200).json({
-            success: true,
-            pid: process.pid
-        })
-    })
-    */
-    
     exec(recordStartCommand, (error, stdout, stderr) => {
         if (error || stderr) {
             const err = error ? error : stderr
@@ -57,7 +59,7 @@ app.post('/recorder/v1/start', (req, res, next) => {
             success: true
         })
     })
-    
+    */
     /*
     RecordManager.start(key, appid, channel).then(recorder => {
         //start recorder success
