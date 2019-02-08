65139f2a5616e75bd58c2f6688e335735a3be854
Tsubasa Oshima <w.tsubasa.physics@gmail.com>
2019-02-08 22:22:03 +0900

    fix

--- server/app.js
+++ server/app.js
@@ -23,16 +23,20 @@ app.post('/recorder/v1/start', (req, res, next) => {
     const process = childProcess.spawn('/home/oshima/withlive-agora-recording/samples/cpp/recorder_local', ['--appId', appid, '--uid',  0,  '--channel', channel,  '--recordFileRootDir',  '/home/oshima/withlive-agora-recording/server/output',  '--appliteDir', '/home/oshima/withlive-agora-recording/bin', '--idle=10', '--isMixingEnabled=1', '--layoutMode=1'])
     process.stdout.on('data', (data) => {
         console.log(`stdout: ${data}`)
+        /*
         res.status(200).json({
             success: true
         })
+        */
     })
       
     process.stderr.on('data', (data) => {
         console.log(`stderr: ${data}`)
+        /*
         res.status(500).json({
             success: false
         })
+        */
     })
     
     process.on('close', (code) => {
