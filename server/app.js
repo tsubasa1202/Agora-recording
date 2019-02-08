0f65bfcb7e40c3909a64a259ecd68f64d0c38386
Tsubasa Oshima <w.tsubasa.physics@gmail.com>
2019-02-08 22:19:39 +0900

    childProcess.spawnを使用

--- server/app.js
+++ server/app.js
@@ -3,11 +3,13 @@ const app = express()
 const port = 80
 // const RecordManager = require('./recordManager')
 const bodyParser = require('body-parser')
-const { exec } = require('child_process')
+// const { exec } = require('child_process')
+const childProcess = require('child_process');
 
 app.use(bodyParser.json());
 
 app.post('/recorder/v1/start', (req, res, next) => {
+    console.log('/recorder/v1/start')
     let { body } = req;
     let { appid, channel, key } = body;
     if (!appid) {
@@ -17,7 +19,27 @@ app.post('/recorder/v1/start', (req, res, next) => {
         throw new Error("channel is mandatory");
     }
     
-    const recordStartCommand = `/home/oshima/withlive-agora-recording/samples/cpp/recorder_local --appId ${appid} --uid 0 --channel ${channel} --recordFileRootDir /home/oshima/withlive-agora-recording/server/output --appliteDir  /home/oshima/withlive-agora-recording/bin  --idle=10 --isMixingEnabled=1 --layoutMode=1 &`
+    // const recordStartCommand = `/home/oshima/withlive-agora-recording/samples/cpp/recorder_local --appId ${appid} --uid 0 --channel ${channel} --recordFileRootDir /home/oshima/withlive-agora-recording/server/output --appliteDir  /home/oshima/withlive-agora-recording/bin  --idle=10 --isMixingEnabled=1 --layoutMode=1 &`
+    const process = childProcess.spawn('/home/oshima/withlive-agora-recording/samples/cpp/recorder_local', ['--appId', appid, '--uid',  0,  '--channel', channel,  '--recordFileRootDir',  '/home/oshima/withlive-agora-recording/server/output',  '--appliteDir', '/home/oshima/withlive-agora-recording/bin', '--idle=10', '--isMixingEnabled=1', '--layoutMode=1'])
+    process.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`)
+        res.status(200).json({
+            success: true
+        })
+    })
+      
+    process.stderr.on('data', (data) => {
+        console.log(`stderr: ${data}`)
+        res.status(500).json({
+            success: false
+        })
+    })
+    
+    process.on('close', (code) => {
+        console.log(`child process exited with code ${code}`)
+    })
+    
+    /*
     exec(recordStartCommand, (error, stdout, stderr) => {
         if (error || stderr) {
             const err = error ? error : stderr
@@ -32,6 +54,7 @@ app.post('/recorder/v1/start', (req, res, next) => {
             success: true
         })
     })
+    */
     /*
     RecordManager.start(key, appid, channel).then(recorder => {
         //start recorder success
