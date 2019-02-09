b6fc80a1abddfa98d072c1221f6e95c4caadc1e3
Tsubasa Oshima <w.tsubasa.physics@gmail.com>
2019-02-10 02:25:24 +0900

    add logger

--- server/app.js
+++ server/app.js
@@ -5,7 +5,11 @@ const port = 8080
 const bodyParser = require('body-parser')
 // const { exec } = require('child_process')
 const childProcess = require('child_process')
-const logger = require('./logger');
+const log4js = require('log4js')
+log4js.configure({
+  appenders: { cheese: { type: 'file', filename: 'access.log' } },
+  categories: { default: { appenders: ['withlive'], level: 'info' } }
+});
 
 app.use(bodyParser.json());
 
@@ -19,12 +23,12 @@ app.post('/recorder/v1/start', (req, res, next) => {
         throw new Error("channel is mandatory");
     }
 
-    logger.request.info(`channel: ${channel}`)
+    logger.info(`channel: ${channel}`)
     const process = childProcess.spawn('/home/tsubasa/withlive-agora-recording/samples/cpp/recorder_local', ['--appId', appid, '--uid',  0,  '--channel', channel,  '--recordFileRootDir',  '/home/tsubasa/withlive-agora-recording/server/output',  '--appliteDir', '/home/tsubasa/withlive-agora-recording/bin', '--idle=10', '--isMixingEnabled=1', '--layoutMode=1'])
     
         process.stdout.on('data', (data) => {
             // console.log(`stdout: ${data}`)
-            logger.request.info(`stdout: ${data}`)
+            logger.info(`stdout: ${data}`)
             const containSuccessJoin = String(data).indexOf(`join channel Id: ${channel}, with uid:`) !== -1
             if(containSuccessJoin){
                 res.status(200).json({
@@ -36,18 +40,18 @@ app.post('/recorder/v1/start', (req, res, next) => {
         
         process.stderr.on('data', (data) => {
             // console.error(`stderr: ${data}`)
-            logger.request.error(`stderr: ${data}`)
+            logger.error(`stderr: ${data}`)
         })
         
         process.on('close', (code) => {
             // console.log(`child process exited with code ${code}`)
-            logger.request.info(`child process exited with code ${code}`)
+            logger.info(`child process exited with code ${code}`)
         })
 })
 
 app.use( (err, req, res, next) => {
    // console.error(err.stack)
-   logger.request.error(err.stack)
+   logger.error(err.stack)
     res.status(500).json({
         success: false,
         err: err.message || 'generic error'
