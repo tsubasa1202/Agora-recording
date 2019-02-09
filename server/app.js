8cdadc6960d0dfe9621afeaa42336c49412fe094
Tsubasa Oshima <w.tsubasa.physics@gmail.com>
2019-02-09 15:01:06 +0900

    logを不要なコードを削除

--- server/app.js
+++ server/app.js
@@ -9,7 +9,6 @@ const childProcess = require('child_process');
 app.use(bodyParser.json());
 
 app.post('/recorder/v1/start', (req, res, next) => {
-    console.log('/recorder/v1/start')
     let { body } = req;
     let { appid, channel, key } = body;
     if (!appid) {
@@ -18,8 +17,6 @@ app.post('/recorder/v1/start', (req, res, next) => {
     if (!channel) {
         throw new Error("channel is mandatory");
     }
-    
-    // const recordStartCommand = `/home/tsubasa/withlive-agora-recording/samples/cpp/recorder_local --appId ${appid} --uid 0 --channel ${channel} --recordFileRootDir /home/tsubasa/withlive-agora-recording/server/output --appliteDir  /home/tsubasa/withlive-agora-recording/bin  --idle=10 --isMixingEnabled=1 --layoutMode=1 &`
     const process = childProcess.spawn('/home/tsubasa/withlive-agora-recording/samples/cpp/recorder_local', ['--appId', appid, '--uid',  0,  '--channel', channel,  '--recordFileRootDir',  '/home/tsubasa/withlive-agora-recording/server/output',  '--appliteDir', '/home/tsubasa/withlive-agora-recording/bin', '--idle=10', '--isMixingEnabled=1', '--layoutMode=1'])
     
         process.stdout.on('data', (data) => {
@@ -40,76 +37,7 @@ app.post('/recorder/v1/start', (req, res, next) => {
         process.on('close', (code) => {
             console.log(`child process exited with code ${code}`)
         })
-
-
-
-    /*
-    const process = childProcess.execFile('/home/tsubasa/withlive-agora-recording/samples/cpp/recorder_local', 
-    ['--appId',appid, '--uid',  0,  '--channel', channel,  '--recordFileRootDir',
-    '/home/tsubasa/withlive-agora-recording/server/output',  '--appliteDir',
-    '/home/tsubasa/withlive-agora-recording/bin', '--idle=10', '--isMixingEnabled=1', '--layoutMode=1', '&'], (error, stdout, stderr) => {
-        
-        if (error) {
-            console.error("stderr", stderr)
-            return res.status(500).json({
-                success: false,
-                pid: process.pid
-            })
-        }
-        // console.log(stdout);
-        // console.log("pid: " +process.pid)
-        return res.status(200).json({
-            success: true,
-            pid: process.pid
-        })
-    })
-    */
-    
-    
-    /*
-    exec(recordStartCommand, (error, stdout, stderr) => {
-        if (error || stderr) {
-            const err = error ? error : stderr
-            console.error(`[ERROR] ${err}`)
-            res.status(500).json({
-                success: false
-            })
-            return
-        }
-        //start recorder success
-        res.status(200).json({
-            success: true
-        })
-    })
-    */
-    /*
-    RecordManager.start(key, appid, channel).then(recorder => {
-        //start recorder success
-        res.status(200).json({
-            success: true,
-            sid: recorder.sid
-        });
-    }).catch((e) => {
-        //start recorder failed
-        next(e);
-    });
-    */
 })
-
-/*
-app.post('/recorder/v1/stop', (req, res, next) => {
-    let { body } = req;
-    let { sid } = body;
-    if (!sid) {
-        throw new Error("sid is mandatory");
-    }
-
-    RecordManager.stop(sid);
-    res.status(200).json({
-        success: true
-    });
-})
-*/
 
 app.use( (err, req, res, next) => {
     console.error(err.stack)
